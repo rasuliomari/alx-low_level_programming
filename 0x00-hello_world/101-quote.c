49734ae804d996e2fcdf69a930f3c7042798b19a
rasuliomari <rasuli707046@gmail.com>
2023-02-16 12:08:30 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", s);
+	return (1);
+}
