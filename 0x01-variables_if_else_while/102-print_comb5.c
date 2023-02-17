96544e1f0eaf51984dcf6946f60e4bac08690897
rasuliomari <rasuli707046@gmail.com>
2023-02-17 13:03:53 -0800

    software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Combination '00' - '99'
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0'); /* print first digit of i */
+			putchar((i % 10) + '0'); /* print second digit of i */
+			putchar(' ');
+			putchar((j / 10) + '0'); /* print first digit of j */
+			putchar((j % 10) + '0'); /*  print second digit of j */
+
+			if (i < 99 || j < 99) /* check if it's the last combination */
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
