ecc7df5f3ebd7377c5dce8cdbbee45d71ee8196a
rasuliomari <rasuli707046@gmail.com>
2023-03-08 10:45:55 -0800

    zero task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string then a new line
+ * @s: pointer to a string
+ *
+ * Return: nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
