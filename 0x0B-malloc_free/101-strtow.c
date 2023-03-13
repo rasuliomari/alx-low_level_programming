1d88e9679b641783a752cc8ee5e45e03f2556a06
rasuliomari <rasuli707046@gmail.com>
2023-03-13 09:56:31 -0700

    number 6 task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -28,37 +28,37 @@ void ch_free_grid(char **grid, unsigned int height)
 char **strtow(char *str)
 {
 	char **aout;
- 	unsigned int x, height, i, j, a1;
+	unsigned int c, height, i, j, a1;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	for (x = height = 0; str[x] != '\0'; x++)
-		if (str[x] != ' ' && (str[x + 1] == ' ' || str[x + 1] == '\0'))
+	for (c = height = 0; str[c] != '\0'; c++)
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			height++;
 	aout = malloc((height + 1) * sizeof(char *));
 	if (aout == NULL || height == 0)
 	{
-	free(aout);
-	return (NULL);
+		free(aout);
+		return (NULL);
 	}
 	for (i = a1 = 0; i < height; i++)
 	{
-		for (x = a1; str[x] != '\0'; x++)
+		for (c = a1; str[c] != '\0'; c++)
 		{
-			if (str[x] == ' ')
+			if (str[c] == ' ')
 				a1++;
-			if (str[x] != ' ' && (str[x + 1] == ' ' || str[x + 1] == '\0'))
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
-				aout[i] = malloc((x - a1 + 2) * sizeof(char));
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
 				if (aout[i] == NULL)
 				{
-				ch_free_grid(aout, i);
-				return (NULL);
+					ch_free_grid(aout, i);
+					return (NULL);
 				}
 				break;
 			}
 		}
-		for (j = 0; a1 <= x; a1++, j++)
+		for (j = 0; a1 <= c; a1++, j++)
 			aout[i][j] = str[a1];
 		aout[i][j] = '\0';
 	}
