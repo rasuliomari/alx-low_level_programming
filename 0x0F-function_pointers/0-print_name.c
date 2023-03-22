fdec8855d91a905824c3870813d95b66d7edc151
rasuliomari <rasuli707046@gmail.com>
2023-03-22 08:05:42 -0700

    zero task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+ /**
+  *print_name - prints a name.
+  *@name: pointer to name.
+  *@f: function pointer.
+  *
+  *Return: void.
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+void _putchar(char *c);
+void print_name(char *name, void (*f)(char *));
+
+
+#endif /*FUNCTION_POINTERS_H*
